39eab017b9ef6fe11e12e594a9bd8dc7a04f39bb
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-20 21:13:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   new_array = ["a", "b", "c", "d"]
-  new_array.push ("e")
+  new_array<< "e"
 end
 
 def add_element_to_start_of_array(array, element)
